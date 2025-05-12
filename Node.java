592fca3aa9982e3227c3c99f76e4f51cc6938afa
profdniel <167190549+profdniel@users.noreply.github.com>
2025-05-12 17:55:10 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,14 @@
+/***
+Coder: Roslan S, UiTM Pahang, roslancs@uitm.edu.my
+Year: 2012
+***/
+
+public class Node<E>{
+    E data;
+    Node next;
+        
+    public Node(E data) {
+        this.data = data;
+    }
+
+}
